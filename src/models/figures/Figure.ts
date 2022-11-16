a7471f899dcce1b1e16c97850ab7f8c3e9a659ff
ScobarDen <gdv-ps@mail.ru>
2022-11-16 15:41:31 +0300

    Добавил фигуры

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,36 @@
-export class Figure{
+import {Colors} from "../Colors";
+import logo from '../../assets/b_bishop_png_shadow_128px.png';
+import {Cell} from "../Cell";
 
+export enum FigureNames {
+    FUGURE = 'Фигура',
+    KING = 'Король',
+    KNIGHT = 'Конь',
+    PAWN = 'Пешка',
+    QUEEN = 'Ферзь',
+    ROOK = 'Ладья',
+    BISHOP = 'Слон',
+}
+
+export class Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number;
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureNames.FUGURE;
+        this.id = Math.random() * Date.now();
+    }
+
+    canMove(target: Cell): boolean {
+        return true;
+    }
+
+    moveFigure(target: Cell){}
 };
---
+++ src/models/figures/Qeen.ts
@@ -0,0 +1,5 @@
+import {Figure} from "./Figure";
+
+export class Qeen extends Figure {
+    
+}
